c8ca17ada9495c56534e97532a2b0d780409b016
Niko Lewis <nikolewis01@gmail.com>
2025-08-28 21:37:49 -0400

    correcting hero img path

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                         </p>
                     </div>
                 </div>
-                <div className="absolute inset-0 z-0 opacity-20 bg-contain bg-cover bg-no-repeat" style={{ backgroundImage: 'url("public/Technologist.webp")'}}></div>
+                <div className="absolute inset-0 z-0 opacity-20 bg-contain bg-cover bg-no-repeat" style={{ backgroundImage: 'url("/Technologist.webp")'}}></div>
             </section>
 
         </>
